2b4cd91dabe4c16b64babef815ac258883c9d0a9
Axif <muhamadasif570@gmail.com>
2025-03-14 02:39:54 +0600

    88

--- backend/services/otpService.js
+++ backend/services/otpService.js
@@ -7,7 +7,7 @@ class OtpService {
       const response = await axios.post('https://api.applink.com.bd/otp/request', {
         applicationId: process.env.APPLICATION_ID,
         password: process.env.PASSWORD,
-        subscriberId: `tel:${phoneNumber}`,
+        subscriberId: `tel:88${phoneNumber}`,
         action: "1"
       }, {
         headers: {
